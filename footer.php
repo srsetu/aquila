256e5a04dec101c86bf69319a5bf546601a74331
Saidur Rahman Setu <srsetu@gmail.com>
2020-10-29 12:34:04 +0600

    Bootstrap navigation added

--- footer.php
+++ footer.php
@@ -5,6 +5,9 @@
  * @package Aquila
  */
 ?>
+
+</div>
+</div>
 <footer>Footer</footer>
 <?php wp_footer(); ?>
 </body>
--- header.php
+++ header.php
@@ -20,4 +20,8 @@ if (function_exists('wp_body_open')) {
     wp_body_open();
 }
 ?>
-<header>Header</header>
+<div id="page" class="site">
+    <header id="masthead" class="site-header" role="banner">
+        <?php get_template_part('template-parts/header/nav'); ?>
+    </header>
+    <div id="content" class="site-content">
